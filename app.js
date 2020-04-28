d3debd7d685f32dfa34e5044dececba70f21ea84
shanemonksobyrne <monksobyrneshane@gmail.com>
2020-04-28 07:30:59 +0100

    small text update

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const sslRedirect = require("heroku-ssl-redirect");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 80;
 
 app.use(express.static("public"));
+app.use(sslRedirect());
 
 app.get("/", (req, res) => {
     res.send("index");
